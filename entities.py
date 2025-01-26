0a57a1b28976dc62e2e094731b84a4df8d355290
simope <simonepecora@live.it>
2025-01-26 10:58:15 +0000

    redefine interaction and eating logic

--- entities.py
+++ entities.py
@@ -18,11 +18,23 @@ class Entity:
         return (abs(self.x - other_entity.x) < self.size and 
                 abs(self.y - other_entity.y) < self.size)
 
-    def eat(self, other_entity: 'Entity'):
+    def interact(self, other_entity: 'Entity'):
         if self.check_collision(other_entity):
-            other_entity.alive = False  # 'Eat' the other entity
-            self.size += 1  # Grow after eating
+            if self.size > other_entity.size: 
+                self.eat_other(other_entity) # This entity eats the other entity
+            elif self.size < other_entity.size:
+                self.get_eaten(other_entity) # This entity gets eaten by the other entity
+            else:
+                random.choice([self.eat_other, self.get_eaten])(other_entity) # Randomly choose who eats who
 
-    def reproduce(self):
-        if random.random() < 0.01:  # Small chance to reproduce
-            return Entity(self.x + 2, self.y + 2, self.speed, self.size)
+    def eat_other(self, other_entity: 'Entity'):
+        other_entity.alive = False # The other entity dies
+        self.size += 1 # This entity grows
+            
+    def get_eaten(self, other_entity: 'Entity'):
+        self.alive = False  # This entity dies
+        other_entity.size += 1  # The other entity grows
+
+    # def reproduce(self):
+    #     if random.random() < 0.01:  # Small chance to reproduce
+    #         return Entity(self.x + 2, self.y + 2, self.speed, self.size)
